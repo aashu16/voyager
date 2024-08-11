a4411d9036cd5eff6fb8b69ba6be3a41a32c8da3
Aashu Anshuman <code@aashu.dev>
2024-08-11 23:02:05 +0100

    Update tests

--- e2e/user-signup.spec.ts
+++ e2e/user-signup.spec.ts
@@ -1,9 +1,5 @@
 import { test, expect } from "@playwright/test";
-import {
-  approveRegistration,
-  listRegistrationApplications,
-  newLemmyClientWithCredentials,
-} from "./init-lemmy-server";
+import { newLemmyClientWithCredentials } from "./init-lemmy-server";
 
 const url = "http://localhost:8536/";
 
@@ -12,11 +8,29 @@ test("create user post", async ({ page }) => {
   await page.waitForURL("/posts/localhost:8536/all");
   await page.goto("/profile");
 
-  const lemmyAdmin = await newLemmyClientWithCredentials(
-    url,
-    "voyager",
-    "voyagerapp",
-  );
+  try {
+    const lemmyAdmin = await newLemmyClientWithCredentials(
+      url,
+      "voyager",
+      "voyagerapp",
+    );
+  } catch (error) {
+    throw new Error("Err:" + error);
+  }
+
+  await expect(
+    page.getByText("You are browsing localhost:8536 as"),
+  ).toBeVisible();
+
+  await page.getByRole("button", { name: "Get Started" }).click();
+
+  await page.getByRole("button", { name: "Join localhost:8536" }).click();
+});
+
+test("create user post 2", async ({ page }) => {
+  await page.goto("/");
+  await page.waitForURL("/posts/localhost:8536/all");
+  await page.goto("/profile");
 
   await expect(
     page.getByText("You are browsing localhost:8536 as"),
